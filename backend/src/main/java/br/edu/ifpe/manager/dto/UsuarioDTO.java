c62b7d702b1b66f35ac0eeab6ea44a78b5972f3f
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-01-19 02:09:11 -0300

    Atualizar arquivos da Entidade Usuario

--- backend/src/main/java/br/edu/ifpe/manager/dto/UsuarioDTO.java
+++ backend/src/main/java/br/edu/ifpe/manager/dto/UsuarioDTO.java
@@ -2,11 +2,9 @@ package br.edu.ifpe.manager.dto;
 
 import br.edu.ifpe.manager.model.TipoUsuario;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
 @AllArgsConstructor
 @Getter
 @Setter
