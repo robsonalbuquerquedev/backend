ae04a9f17e9b1dd9d050a3440f2149dd6e4133ed
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-01-21 02:03:40 -0300

    Atualizar arquivo BackendApplication.java

--- backend/src/main/java/br/edu/ifpe/manager/BackendApplication.java
+++ backend/src/main/java/br/edu/ifpe/manager/BackendApplication.java
@@ -2,25 +2,27 @@ package br.edu.ifpe.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-import br.edu.ifpe.manager.config.ConfigTest;
+/*import br.edu.ifpe.manager.config.ConfigTest;*/
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+/*import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;*/
 
 @SpringBootApplication
-public class BackendApplication implements CommandLineRunner {
+@EnableScheduling
+public class BackendApplication /*implements CommandLineRunner*/ {
 
-    @Autowired
-    private ConfigTest configTest;
+    /*@Autowired
+    private ConfigTest configTest;*/
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-    @Override
+    /*@Override
     public void run(String... args) throws Exception {
         // Chama o método que imprime o valor da chave secreta
         configTest.printSecret();
-    }
+    }*/
 }
